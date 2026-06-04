2f1a890b210ef400de03291cbb5b8c328e957383
himanxhu27modanwal-ai <himanxhu27modanwal@gmail.com>
2026-06-04 17:51:20 +0530

    summer assignment day 3

---
+++ nthfibonacciterm.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,a=0,b=1,c,i;
+    printf("entr the value of n:");
+    scanf("%d",&n);
+    if(n==1)
+    printf("the nth fibonacci term=%d",a);
+    else if(n==2)
+    printf("the nth fibonacci term=%d",b);
+    else
+    {
+        for(i=3;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        printf("nth fibonacci term=%d",b);
+    }
+    return 0;
+
+}
