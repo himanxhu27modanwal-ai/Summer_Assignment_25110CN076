198f46aa5ab08d8915664c8f8e9b0ebec340778b
himanxhu27modanwal-ai <himanxhu27modanwal@gmail.com>
2026-06-23 17:45:26 +0530

    summer assignment Day22

---
+++ Day22/characterfrequency.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    char str[100], ch;
+    int i = 0, count = 0;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    while(str[i] != '\0')
+    {
+        if(str[i] == ch)
+        {
+            count++;
+        }
+        i++;
+    }
+    printf("Frequency of %c = %d", ch, count);
+    return 0;
+}
---
+++ Day22/countwordsinsentence.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i = 0, words = 1;
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+    while(str[i] != '\0')
+    {
+        if(str[i] == ' ')
+        {
+            words++;
+        }
+        i++;
+    }
+    printf("Total words = %d", words);
+    return 0;
+}
